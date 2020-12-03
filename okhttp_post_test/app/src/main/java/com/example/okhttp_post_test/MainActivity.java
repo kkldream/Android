0c9a2e3cc70328c43520a7c6f57fbf20c73cee09
kkldream <kk69347321@gmail.com>
2020-12-03 19:38:49 +0800

    update

---
+++ okhttp_post_test/app/src/main/java/com/example/okhttp_post_test/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.okhttp_post_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
