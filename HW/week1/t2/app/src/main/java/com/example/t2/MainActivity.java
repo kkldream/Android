c8895903496543d0e14ba1f2b26c0ad022641406
kkldream <kk69347321@gmail.com>
2021-02-24 20:31:16 +0800

    hw2完成

--- HW/week1/t2/app/src/main/java/com/example/t2/MainActivity.java
+++ HW/week1/t2/app/src/main/java/com/example/t2/MainActivity.java
@@ -3,12 +3,24 @@ package com.example.t2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText editText;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
+        textView = findViewById(R.id.textView);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setText(editText.getText() + ", 你好");
+            }
+        });
     }
 }
