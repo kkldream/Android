3318bacb442987d4fe6ff03b0402e9199a273356
kkldream <kk69347321@gmail.com>
2021-02-24 17:47:56 +0800

    hw1完成

--- HW/week1/t1/app/src/main/java/com/example/t1/MainActivity.java
+++ HW/week1/t1/app/src/main/java/com/example/t1/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.t1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    float size;
+    TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(100);
+                    size = textView.getTextSize();
+                    Log.i("TAG", String.valueOf(size));
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                textView.setTextSize(++size);
+                Log.i("TAG", String.valueOf(size));
+            }
+        });
     }
 }
