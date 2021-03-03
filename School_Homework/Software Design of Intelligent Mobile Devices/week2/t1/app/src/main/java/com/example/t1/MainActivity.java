02292e0879361163e7a83e2fd7150b7cdef4164b
kkldream <kk69347321@gmail.com>
2021-03-03 16:13:54 +0800

    week2 hw1 finish

--- School_Homework/Software Design of Intelligent Mobile Devices/week2/t1/app/src/main/java/com/example/t1/MainActivity.java
+++ School_Homework/Software Design of Intelligent Mobile Devices/week2/t1/app/src/main/java/com/example/t1/MainActivity.java
@@ -3,12 +3,25 @@ package com.example.t1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    EditText editText1, editText2, editText3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView4);
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        editText3 = findViewById(R.id.editTextPhone);
+    }
+
+    public void button_onClick(View view) {
+        String str = "姓：" + editText1.getText() + "\n名：" + editText2.getText() + "\n電話：" + editText3.getText();
+        textView.setText(str);
     }
 }
