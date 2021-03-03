431d927506efb83ddfbea838fe6a8899d602b8d6
kkldream <kk69347321@gmail.com>
2021-03-03 17:52:55 +0800

    t3 finish

--- School_Homework/Software Design of Intelligent Mobile Devices/week2/t3/app/src/main/java/com/example/t3/MainActivity.java
+++ School_Homework/Software Design of Intelligent Mobile Devices/week2/t3/app/src/main/java/com/example/t3/MainActivity.java
@@ -3,12 +3,36 @@ package com.example.t3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView;
+    Button button;
+    int Num = 0;
+    Boolean button_LongClick = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!button_LongClick) textView.setText(String.valueOf(++Num));
+                button_LongClick = false;
+            }
+        });
+        button.setOnLongClickListener(new View.OnLongClickListener() {
+            @Override
+            public boolean onLongClick(View view) {
+                Num = 0;
+                button_LongClick = true;
+                textView.setText("0");
+                return false;
+            }
+        });
     }
 }
