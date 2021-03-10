daa3646ee79da1105efe20791c71170374bbba9f
kkldream <kk69347321@gmail.com>
2021-03-10 17:07:51 +0800

    finish week3 t1

--- School_Homework/Software Design of Intelligent Mobile Devices/week3/t1/app/src/main/java/com/example/t1/MainActivity.java
+++ School_Homework/Software Design of Intelligent Mobile Devices/week3/t1/app/src/main/java/com/example/t1/MainActivity.java
@@ -2,13 +2,45 @@ package com.example.t1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Vibrator;
+import android.util.Log;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class MainActivity extends AppCompatActivity implements View.OnTouchListener {
+    TextView textView;
+    Vibrator vibrator;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+        textView.setOnTouchListener(this);
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+                vibrator.vibrate(5000);
+                Log.i("TAG","ACTION_DOWN");
+                Toast.makeText(this,"ACTION_DOWN",Toast.LENGTH_SHORT).show();
+                break;
+            case MotionEvent.ACTION_UP:
+            case MotionEvent.ACTION_CANCEL:
+                vibrator.cancel();
+                Log.i("TAG","ACTION_UP");
+                Toast.makeText(this,"ACTION_UP",Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 }
