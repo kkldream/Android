99fbe492a0b75b7781b597b1fa40a19273366e63
kkldream <kk69347321@gmail.com>
2021-03-16 15:59:49 +0800

    y ok

---
+++ School_Homework/Software Design of Intelligent Mobile Devices/week3/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,50 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class MainActivity extends AppCompatActivity {
+    ImageView imageView;
+    Bitmap baseBitmap;
+    Canvas canvas;
+    Paint paint;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        imageView = findViewById(R.id.imageView);
+        paint = new Paint();
+        paint.setStrokeWidth(100);
+        paint.setColor(Color.RED);
+        imageView.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent motionEvent) {
+                if (motionEvent.getAction() == MotionEvent.ACTION_DOWN) {
+                    if (baseBitmap == null) {
+                        baseBitmap = Bitmap.createBitmap(imageView.getWidth(), imageView.getHeight(), Bitmap.Config.ARGB_8888);
+                        canvas = new Canvas(baseBitmap);
+                        canvas.drawColor(Color.WHITE);
+                        imageView.setImageBitmap(baseBitmap);
+                    }
+                    float cx = motionEvent.getX();
+                    float cy = motionEvent.getY();
+                    canvas.drawCircle(cx, cy, 10, paint);
+                    imageView.setImageBitmap(baseBitmap);
+                }
+                return true;
+            }
+        });
+    }
+}
