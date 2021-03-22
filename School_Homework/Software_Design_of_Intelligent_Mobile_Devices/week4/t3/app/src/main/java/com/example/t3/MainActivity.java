bb673013ef0ef13ff9a278c9d2f08ab8be4fadc8
kkldream <kk69347321@gmail.com>
2021-03-22 16:45:11 +0800

    week4 t3 ok

--- School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week4/t3/app/src/main/java/com/example/t3/MainActivity.java
+++ School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week4/t3/app/src/main/java/com/example/t3/MainActivity.java
@@ -3,12 +3,50 @@ package com.example.t3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    Spinner spinner;
+    Button button;
+    TextView textView1, textView2, textView3, textView4;
+    EditText editText1, editText2;
+    int mode = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        spinner = findViewById(R.id.spinner);
+        button = findViewById(R.id.button);
+        textView1 = findViewById(R.id.textView);
+        textView2 = findViewById(R.id.textView2);
+        textView3 = findViewById(R.id.textView3);
+        textView4 = findViewById(R.id.textView4);
+        editText1 = findViewById(R.id.editTextTextPersonName);
+        editText2 = findViewById(R.id.editTextTextPersonName2);
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                mode = i;
+                textView1.setText("YYYYYYYYYYYYYY：" + (i * 1.1));
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                float ans = (float) (Integer.valueOf(editText1.getText().toString()) * Integer.valueOf(editText1.getText().toString()) * (mode * 1.1));
+                textView4.setText("Ans：" + ans);
+            }
+        });
     }
 }
