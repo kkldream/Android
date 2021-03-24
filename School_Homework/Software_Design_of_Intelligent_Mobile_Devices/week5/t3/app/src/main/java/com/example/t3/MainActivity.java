cacfea6542db1507c82e7d109639ed657edd51a1
kkldream <kk69347321@gmail.com>
2021-03-24 17:30:12 +0800

    complete week5 t3

--- School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week5/t3/app/src/main/java/com/example/t3/MainActivity.java
+++ School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week5/t3/app/src/main/java/com/example/t3/MainActivity.java
@@ -2,13 +2,44 @@ package com.example.t3;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
+import android.app.TimePickerDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.TextView;
+import android.widget.TimePicker;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView1, textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        textView1 = findViewById(R.id.textView1);
+        textView2 = findViewById(R.id.textView2);
+        textView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+                    @Override
+                    public void onDateSet(DatePicker datePicker, int i, int i1, int i2) {
+                        textView1.setText("日期：" + i + " / " + i1 + " / " + i2);
+                    }
+                }, 2021, 3, 24).show();
+            }
+        });
+        textView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new TimePickerDialog(MainActivity.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int i, int i1) {
+                        textView2.setText("時間：" + i + " : " + i1);
+                    }
+                }, 0,0,true).show();
+            }
+        });
     }
 }
