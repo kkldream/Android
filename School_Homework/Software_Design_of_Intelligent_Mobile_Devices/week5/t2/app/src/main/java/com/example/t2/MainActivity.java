56d78eb8771ec78c60c9aa62b050762a84bea685
kkldream <kk69347321@gmail.com>
2021-03-24 17:11:21 +0800

    complete week5 t2

--- School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week5/t2/app/src/main/java/com/example/t2/MainActivity.java
+++ School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week5/t2/app/src/main/java/com/example/t2/MainActivity.java
@@ -3,12 +3,37 @@ package com.example.t2;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
-
+    LinearLayout linearLayout;
+    ListView listView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        linearLayout = findViewById(R.id.root);
+        listView = findViewById(R.id.lv);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                switch (i) {
+                    case 0:
+                        Snackbar.make(linearLayout, "AAAAAAAAAAAAAAA", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case 1:
+                        Snackbar.make(linearLayout, "BBBBBBBBBBBBB", Snackbar.LENGTH_SHORT).show();
+                        break;
+                    case 2:
+                        Snackbar.make(linearLayout, "CCCCCCCCCCCCC", Snackbar.LENGTH_SHORT).show();
+                        break;
+                }
+            }
+        });
     }
 }
