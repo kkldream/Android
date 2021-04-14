b4691f3628e780ae3ac1fc82b5ca2529b70537c0
kkldream <kk69347321@gmail.com>
2021-04-14 15:23:03 +0800

    add week8 t1

--- android_storage/app/src/main/java/com/example/android_storage/MainActivity.java
+++ android_storage/app/src/main/java/com/example/android_storage/MainActivity.java
@@ -2,13 +2,41 @@ package com.example.android_storage;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.content.Context;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "TAG";
+    private static final int WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+    }
+    public void button1_onClick(View view) {
+        Log.i(TAG, "----------------------------------------------------------------------------------------------------");
+        Log.i(TAG, "getFilesDir：" + getFilesDir());
+        Log.i(TAG, "getNoBackupFilesDir：" + getNoBackupFilesDir());
+        Log.i(TAG, "Environment.getExternalStorageDirectory：" + Environment.getExternalStorageDirectory());
+        Log.i(TAG, "getExternalFilesDir：" + getExternalFilesDir(null));
+    }
+    public void button2_onClick(View view) {
+        File sdCard = Environment.getExternalStorageDirectory();
+        File directory_pictures = new File(sdCard, "Pictures");
+        Log.i(TAG,"directory_pictures="+directory_pictures);
     }
 }
