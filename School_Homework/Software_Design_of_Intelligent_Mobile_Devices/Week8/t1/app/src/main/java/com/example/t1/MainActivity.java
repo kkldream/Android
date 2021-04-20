6baa56e9efd6635c12ea124a2406848f43bd4656
kkldream <kk69347321@gmail.com>
2021-04-20 14:37:00 +0800

    f week8 t1

--- School_Homework/Software_Design_of_Intelligent_Mobile_Devices/Week8/t1/app/src/main/java/com/example/t1/MainActivity.java
+++ School_Homework/Software_Design_of_Intelligent_Mobile_Devices/Week8/t1/app/src/main/java/com/example/t1/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.t1;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final int TAKE_PHOTOS_CODE = 1000;
+    private static final int MY_CAMERA_REQUEST_CODE = 100;
+    private static final int MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE = 100;
+    Button button1, button2;
+    ImageView imageView;
+    String savePath = "image";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, MY_CAMERA_REQUEST_CODE);
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, MY_WRITE_EXTERNAL_STORAGE_REQUEST_CODE);
+        }
+        String localPath = Environment.getExternalStorageDirectory() + savePath; // 拼接路径字符串
+        File localFile = new File(localPath);
+        if (!localFile.exists()) { // 判断目录是否存在
+            localFile.mkdirs(); // 新建文件目录
+        }
+        button1 = findViewById(R.id.button);
+        button2 = findViewById(R.id.button2);
+        imageView = findViewById(R.id.imageView);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent, TAKE_PHOTOS_CODE);
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                saveImage("Test.jpg");
+            }
+        });
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_PHOTOS_CODE && resultCode == RESULT_OK) {
+            Bitmap bm = (Bitmap) data.getExtras().get("data");
+            imageView.setImageBitmap(bm);
+        }
+    }
+    public static String getSDPath()
+    {
+        boolean hasSDCard = Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED);
+        if(hasSDCard)
+        {
+            return Environment.getExternalStorageDirectory().toString() + "/saving_picture";
+        }
+        else
+            return "/data/data/com.example.imageviewsave2bitmap/saving_picture";
+    }
+
+    public static Bitmap convertViewToBitmap(View view)
+    {
+        view.measure(View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED), View.MeasureSpec.makeMeasureSpec(0, View.MeasureSpec.UNSPECIFIED));
+        view.layout(0, 0, view.getMeasuredWidth(), view.getMeasuredHeight());
+        view.buildDrawingCache();
+        Bitmap bitmap = view.getDrawingCache();
+
+        return bitmap;
+    }
+
+    public void saveImage(String strFileName)
+    {
+        Bitmap bitmap = convertViewToBitmap(imageView);
+        String strPath = getSDPath();
+
+        try
+        {
+            File destDir = new File(strPath);
+            if (!destDir.exists())
+            {
+                Log.d("MagicMirror", "Dir not exist create it " + strPath);
+                destDir.mkdirs();
+                Log.d("MagicMirror", "Make dir success: " + strPath);
+            }
+
+            File imageFile = new File(strPath + "/" + strFileName);
+            imageFile.createNewFile();
+            FileOutputStream fos = new FileOutputStream(imageFile);
+            bitmap.compress(Bitmap.CompressFormat.JPEG, 50, fos);
+            fos.flush();
+            fos.close();
+        }
+        catch (FileNotFoundException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        catch (IOException e)
+        {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
     }
 }
