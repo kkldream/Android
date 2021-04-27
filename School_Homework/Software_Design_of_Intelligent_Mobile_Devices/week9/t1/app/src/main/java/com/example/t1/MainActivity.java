bb49d99b8a8476b3306c3968ca3e98130f0e9b4a
kkldream <kk69347321@gmail.com>
2021-04-27 14:23:27 +0800

    t1 ok

--- School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week9/t1/app/src/main/java/com/example/t1/MainActivity.java
+++ School_Homework/Software_Design_of_Intelligent_Mobile_Devices/week9/t1/app/src/main/java/com/example/t1/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.t1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK);
+        webView.loadUrl("https://www.youtube.com/embed/pcKcCUsa_04");
     }
 }
